c3c56015d6ae75b1e6a626a034d00caf30f41668
thailane <thailane.araujo@gmail.com>
2023-01-17 21:11:42 -0300

    Main

---
+++ validação-formulários-em aula/script.js
@@ -0,0 +1,109 @@
+// ---------- FUNÇÕES GERAIS -------------- //
+function togglePopup(input, label) {
+  // Mostrar popup de campo obrigatório
+  input.addEventListener("focus", () => {
+    label.classList.add("required-popup");
+  });
+
+  // Ocultar popup de campo obrigatório
+  input.addEventListener("blur", () => {
+    label.classList.remove("required-popup");
+  });
+}
+
+function estilizarInputCorreto(input, helper) {
+  helper.classList.remove("visible");
+  input.classList.remove("error");
+  input.classList.add("correct");
+}
+
+function estilizarInputIncorreto(input, helper) {
+  helper.classList.add("visible");
+  input.classList.add("error");
+  input.classList.remove("correct");
+}
+
+// ---------- VALIDAÇÃO USERNAME ---------- //
+let usernameInput = document.getElementById("username");
+let usernameLabel = document.querySelector('label[for="username"]');
+let usernameHelper = document.getElementById("username-helper");
+
+togglePopup(usernameInput, usernameLabel)
+
+// Validar valor do input
+usernameInput.addEventListener("change", (e)=> {
+  let valor = e.target.value
+
+  if(valor.length < 3){
+    // Adicionar estilos dinâmicos se o valor tiver menos de 3 caracteres
+    usernameHelper.innerText = "Seu username precisa ter 3 ou mais caracteres";
+    estilizarInputIncorreto(usernameInput, usernameHelper)
+  } else {
+    // Adicionar estilos dinâmicos se o valor estiver correto
+    estilizarInputCorreto(usernameInput, usernameHelper);
+  }
+})
+
+// ---------- VALIDAÇÃO EMAIL ---------- //
+let emailInput = document.getElementById("email");
+let emailLabel = document.querySelector('label[for="email"]');
+let emailHelper = document.getElementById("email-helper");
+
+togglePopup(emailInput, emailLabel)
+
+// Validar valor do input
+emailInput.addEventListener("change", (e)=> {
+  let valor = e.target.value
+
+  if(valor.includes("@") && valor.includes(".com")){
+    // Adicionar estilos dinâmicos se o valor estiver correto
+    estilizarInputCorreto(emailInput, emailHelper);
+  } else {
+    // Adicionar estilos dinâmicos se o valor tiver menos de 3 caracteres
+    emailHelper.innerText = "Precisa inserir um email válido";
+    estilizarInputIncorreto(emailInput, emailHelper);
+  }
+})
+
+// ---------- VALIDAÇÃO SENHA ---------- //
+let senhaInput = document.getElementById("senha");
+let senhaLabel = document.querySelector('label[for="senha"]');
+let senhaHelper = document.getElementById("senha-helper");
+
+togglePopup(senhaInput, senhaLabel)
+
+senhaInput.addEventListener("blur",(e)=>{
+togglePopup(senhaInput , senhaLabel)
+senhaInput.addEventListener("blur", (e)=>{
+  let valor = e.target.value
+  if(valor.length == 0){
+    senhaHelper.innerText = "Precisa inserir a senha"
+    estilizarInputIncorreto(senhaInput, senhaHelper)
+
+  }else{
+    estilizarInputCorreto(senhaInput ,senhaHelper)
+  }
+})
+})
+
+//-----------VALIDAÇÃO CONFIRMA SENHA ---------- //
+
+let confirmaSenhaInput = document.getElementById("confirma-senha");
+let confirmaSenhaLabel = document.querySelector('label[for="confirma-senha"]');
+let confirmaSenhaHelper = document.getElementById("confirma-senha-helper");
+
+togglePopup(confirmaSenhaInput, confirmaSenhaLabel);
+
+confirmaSenhaInput.addEventListener("blur", (e)=> {
+  let cSenha = e.target.value;
+  console.log(cSenha + "cSenha");
+    if(cSenha == valor){
+      console.log("Senhas Iguais");
+      senhaHelper.innerText = "Precisa inserir senha valida";
+      estilizarInputIncorreto(confirmaSenhaInput,confirmaSenhaHelper);
+    }else{
+      console.log("Senhas diferentes");
+      estilizarInputCorreto(confirmaSenhaInput,confirmaSenhaHelper);
+    }
+    
+})
